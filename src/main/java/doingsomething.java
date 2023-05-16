1d4f649232df434927627e8b6cb760cd3edcb023
kbhargava <kbhargava@suite42.in>
2023-05-16 15:44:08 +0530

    adding the bit problem in java code

--- src/main/java/doingsomething.java
+++ src/main/java/doingsomething.java
@@ -1,5 +1,15 @@
 class doingsomething {
-    public static void main(){
-        System.out.println("bhargava");
+    public static void main(String[] args){
+        String bits="00000010100101000001111010011100";
+        char[] k=bits.toCharArray();
+        double sum=0;
+        int j=0;
+        for(int i=bits.length()-1;i>=0;i--){
+            if(k[i]=='1'){
+                sum=sum+Math.pow(2,j);
+            }
+            j++;
+        }
+        System.out.println(sum);
     }
 }
