2b7f18219d7f0b50cf5633d422aac09a4c265f21
kbhargava <kbhargava@suite42.in>
2023-08-26 12:52:58 +0530

    update api for text to pdf in java supported en, hi, kn, te

--- src/main/java/com/spring/data/rest/springdatarust/TextToPdfController.java
+++ src/main/java/com/spring/data/rest/springdatarust/TextToPdfController.java
@@ -1,62 +1,119 @@
 package com.spring.data.rest.springdatarust;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.detectlanguage.errors.APIError;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
-
-import static com.itextpdf.tool.xml.css.CSS.Property.FONT;
 
 @RestController
 public class TextToPdfController {
         @PostMapping("/texToPdf")
-        public ResponseEntity<?> getPdfToTextResponse(@RequestBody PdfToTextDto dto) throws DocumentException, IOException {
+        public ResponseEntity<?> getPdfToTextResponse(@RequestBody PdfToTextDto dto) throws IOException, APIError {
             return ResponseEntity.ok(getTextToPdf(dto));
         }
-        private TextToPdfResponse getTextToPdf(PdfToTextDto dto) throws IOException, DocumentException {
+        private TextToPdfResponse getTextToPdf(PdfToTextDto dto) throws IOException, APIError {
             if(!List.of("en","te","kn","tm","hi").contains(dto.getLang())){
-                return TextToPdfResponse.builder()
-                        .success("0")
-                        .message("Error in generating pdf as language is wrong")
-                        .build();
+                return invalidResponse();
             }
-            Document document=new Document();
             String dir = System.getProperty("user.dir");
             String path=dir+"/text.pdf";
-            PdfWriter.getInstance(document,new FileOutputStream(path));
-            PdfWriter.getInstance(document, new FileOutputStream(path));
-            if(dto.getLang().equalsIgnoreCase("hi")) {
-                FontFactory.register("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/Mangal Regular.ttf");
+
+            PdfWriter pdfWriter=new PdfWriter(path);
+
+            PdfDocument pdfDocument=new PdfDocument(pdfWriter);
+            pdfDocument.addNewPage();
+            Document document=new Document(pdfDocument);
+            PdfFont fontLanguage=null;
+            if(dto.getLang().equals("en")){
+                if(checkLanguage(dto.getContent(), dto.getLang())){
+                    return invalidResponse();
+                }
             }
-            if(dto.getLang().equalsIgnoreCase("te")){
-                FontFactory.register("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/Ponnala Regular.otf");
+            if(dto.getLang().equals("hi")) { // hindi
+                if(checkLanguage(dto.getContent(), dto.getLang())){
+                    return invalidResponse();
+                }
+                fontLanguage = PdfFontFactory.createFont("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/NotoSans-Regular.ttf");
+                // NotoSans-Black.ttf
             }
-            if(dto.getLang().equalsIgnoreCase("kn")){
-                FontFactory.register("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/TiroKannada-Regular.ttf");
+            if(dto.getLang().equals("kn")){ // karnataka
+                if(checkLanguage(dto.getContent(), dto.getLang())){
+                    return invalidResponse();
+                }
+                fontLanguage=PdfFontFactory.createFont("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/NotoSerifKannada-VariableFont_wght.ttf");
+            }
+            if(dto.getLang().equals("te")){ // telugu
+                if(checkLanguage(dto.getContent(), dto.getLang())){
+                    return invalidResponse();
+                }
+                fontLanguage=PdfFontFactory.createFont("/Users/bhargavak/Documents/learning/springdatarust/src/main/resources/gautami.ttf");
+            }
+            Paragraph paragraph=new Paragraph();
+            paragraph.setFontSize(10);
+            if(dto.getLang().equalsIgnoreCase("en")){
+                paragraph.add(dto.getContent());
+            }else {
+                paragraph.add(dto.getContent()).setFont(fontLanguage);
             }
 
-            document.open();
-            Font f1 =FontFactory.getFont("Mangal", BaseFont.IDENTITY_H, true);
-            Phrase p = new Phrase(dto.getContent(),f1);
-            document.add(p);
+            document.add(paragraph);
             document.close();
             return TextToPdfResponse.builder()
                     .success("1")
                     .message("successfully created the pdf")
                     .data(path)
                     .build();
+    }
+
+    private Boolean checkLanguage(String text, String language){
+            boolean isLanguageCheck=false;
+           if(language.equalsIgnoreCase("te")) {
+               for (int i = 0; i < text.length(); i++) {
+                   char c = text.charAt(i);
+                   if (!((3072 <= c && c <= 3199) || (c <= 1023 && c > 0))) {
+                       isLanguageCheck= true;
+                   }
+               }
+           }
+           if (language.equalsIgnoreCase("kn")){
+               for (int i = 0; i < text.length(); i++) {
+                   char c = text.charAt(i);
+                   if (!((3327 >= c && c >= 3200) || (c <= 1023 && c >0))) {
+                       isLanguageCheck= true;
+                   }
+               }
+           }
+           if(language.equalsIgnoreCase("hi")){ //2304-2431
+               for (int i = 0; i < text.length(); i++) {
+                   char c = text.charAt(i);
+                   if (!((2304 <= c && c <= 2431) || (c <= 1023 && c >0))) {
+                       isLanguageCheck= true;
+                   }
+               }
+           }
+           if(language.equalsIgnoreCase("en")){
+               for (int i = 0; i < text.length(); i++) {
+                   char c = text.charAt(i);
+                   if (!(c <= 1023 && c >0)) {
+                       isLanguageCheck= true;
+                   }
+               }
+           }
+           return isLanguageCheck;
+    }
+    private TextToPdfResponse invalidResponse(){
+      return  TextToPdfResponse.builder()
+                .success("0")
+                .message("Error in generating pdf as language is wrong")
+                .build();
     }
 }
